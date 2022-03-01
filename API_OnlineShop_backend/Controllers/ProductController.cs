ab7d5d645b06d287cec69720daafcaa3c05b968a
Anii555 <13anna96@gmail.com>
2022-03-01 18:49:27 +0400

    Добавьте файлы проекта.

---
+++ API_OnlineShop_backend/Controllers/ProductController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API_OnlineShop_backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+    //    private static readonly string[] Summaries = new[]
+    //       {
+    //    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    //};
+
+        private readonly ILogger<ProductController> _logger;
+
+        public ProductController(ILogger<ProductController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetProduct")]
+        public IEnumerable<Product> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new Product
+            {
+                ProductId = Random.Shared.Next(0, 55),
+                //ProductName = Random.Shared.Next(0,55),
+                UnitPrice = Random.Shared.Next(0, 55)
+            })
+            .ToArray();
+        }
+    }
+}
