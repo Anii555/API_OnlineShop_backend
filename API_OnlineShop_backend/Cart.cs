89c70f1072ada71aa8dc2f34f3ff450d07de2496
Anii <13anna96@gmail.com>
2022-05-30 18:19:00 +0300

    Класс корзины

---
+++ API_OnlineShop_backend/Cart.cs
@@ -0,0 +1,14 @@
+﻿namespace API_OnlineShop_backend
+{
+    public partial class Cart
+    {
+        public Cart()
+        {
+            Products = new HashSet<Product>();
+        }
+        public int CartItemId { get; set; }
+        public int QuantityCartItem { get; set; }
+
+        public virtual ICollection<Product> Products { get; set; }
+    }
+}
