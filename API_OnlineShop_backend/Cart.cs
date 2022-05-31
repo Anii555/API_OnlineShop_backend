f866104d8bd4db848f7717478b48de42e59a7c54
Anii <13anna96@gmail.com>
2022-05-31 20:14:19 +0300

    Исправление корзины

--- API_OnlineShop_backend/Cart.cs
+++ API_OnlineShop_backend/Cart.cs
@@ -1,14 +1,11 @@
 ﻿namespace API_OnlineShop_backend
 {
-    public partial class Cart
+    public static class Cart
     {
-        public Cart()
-        {
-            Products = new HashSet<Product>();
-        }
-        public int CartItemId { get; set; }
-        public int QuantityCartItem { get; set; }
+        public static int CartItemId { get; set; }
 
-        public virtual ICollection<Product> Products { get; set; }
+        public static int QuantityCartItem { get; set; }
+
+        public static List<Product>? Products { get; set; }
     }
 }
