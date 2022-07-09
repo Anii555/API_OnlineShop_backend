b363fcfe6a4e569e054529de2b087d44d55186db
Anii <13anna96@gmail.com>
2022-07-09 18:16:00 +0300

    Пуш библиотеки, УРА!

---
+++ ProductsLibrary/ProductsLibrary/ProductRepository.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProductsLibrary
+{
+    public class ProductRepository : IdentityDbContext<IdentityUser, IdentityRole, string> //Guid
+    {
+        private readonly NorthwindContext _context;
+
+        public ProductRepository(NorthwindContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Product>> GetAll()
+        {
+            return await _context.Products.ToListAsync();
+        }
+
+        public async Task<Product> GetId(int id)
+        {
+            var prod = await _context.Products.FirstOrDefaultAsync(x => x.ProductId == id);
+
+            if (prod == null)
+            {
+                return null;
+            }
+            else
+            {
+                return prod;
+            }
+        }
+    }
+}
